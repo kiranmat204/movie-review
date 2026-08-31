557dc2ab6269db1b9338a435cebf7039b1d36b2f
kiranmat204 <kiranm1904@gmail.com>
2026-08-31 22:47:35 +1200

    Set up Spring Boot movie review API

---
+++ src/main/java/com/example/movie_review/controller/MovieController.java
@@ -0,0 +1,33 @@
+package com.example.movie_review.controller;
+
+import com.example.movie_review.model.Movie;
+import com.example.movie_review.repository.MovieRepository;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/movies")
+public class MovieController {
+
+    private final MovieRepository movieRepository;
+
+    public MovieController(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    @GetMapping
+    public List<Movie> getAllMovies() {
+        return movieRepository.findAll();
+    }
+
+    @PostMapping
+    public Movie createMovie(@RequestBody Movie movie) {
+        return movieRepository.save(movie);
+    }
+}
